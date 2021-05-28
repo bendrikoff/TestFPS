9a3e06a34737ff7181d0af8f85547233ae3ef0c9
bendrikoff <bendrikoff@gmail.com>
2021-05-28 19:25:03 +0300

    Add fireballs

---
+++ Assets/Fireball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed=10.0f;
+    public int damage = 1;
+
+    private void OnTriggerEnter(Collider other) {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+
+        if(player!=null)
+        {
+            print("player hit");
+        }
+        Destroy(this.gameObject);
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(0,0,speed*Time.deltaTime);
+    }
+}
